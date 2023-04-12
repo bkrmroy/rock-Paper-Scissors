7d9a38ec2fb6ee3924ea2e486c7263fd742ce4ba
bkrmroy <110381880+bkrmroy@users.noreply.github.com>
2023-04-12 17:37:32 +0600

    create a function that plays the game
    created a playRound() function that plays 1 single round of game

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
-let choice = ['rock','paper','scissors'];
+let choice = ["rock","paper","scissors"];
 
 //generate a computer choice
 function getComputerChoice(computerSelection){
     computerSelection = choice[(Math.floor(Math.random() * choice.length))];
     return computerSelection;
 }
-console.log(getComputerChoice());
+//declare both choices
+const computerChoice = getComputerChoice();
+const playerChoice = "scissors"; //temporary, will add user prompt later
+
+console.log(`Computer Choice = ${computerChoice}`);
+console.log(`Player Choice = ${playerChoice}`);
+
+//create the main function that will help to play the game
+function playRound(_computerChoice = computerChoice, _playerChoice = playerChoice){
+    let result;
+    if(_computerChoice === _playerChoice){
+        result = "It's a tie";
+    }
+    else if(_computerChoice === "scissors" && _playerChoice === "rock"){
+        result = "You win";
+    }
+    else if(_computerChoice === "paper" && _playerChoice === "scissors"){
+        result = "You win";
+    }
+    else if(_computerChoice === "rock" && _playerChoice === "paper"){
+        result = "You win";
+    }
+    else{
+        result = "You lose, Computer wins";
+    }
+    return result;
+}
+
+console.log(playRound());
