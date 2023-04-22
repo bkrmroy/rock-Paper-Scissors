76c8613dbcaadf0cc28e5bbec021628fe8b185c3
bkrmroy <110381880+bkrmroy@users.noreply.github.com>
2023-04-22 14:22:50 +0600

    create funtion to play the game and declare result
    created a playGame funtion which plays the game five times using the playRound function
    created a function to decide the winner

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 let choice = ["rock","paper","scissors"];
+let result;
+//one round of the game
+function playRound(_computerChoice, _playerChoice){
+    _playerChoice = prompt("Enter your choice","").toLowerCase();
+    console.log(`Player Choice = ${_playerChoice}`);
 
-//generate a computer choice
-function getComputerChoice(computerSelection){
-    computerSelection = choice[(Math.floor(Math.random() * choice.length))];
-    return computerSelection;
-}
-//declare both choices
-const computerChoice = getComputerChoice();
-const playerChoice = "scissors"; //temporary, will add user prompt later
+    function getComputerChoice(x){
+        x = choice[(Math.floor(Math.random() * choice.length))];
+        return x;
+    }
 
-console.log(`Computer Choice = ${computerChoice}`);
-console.log(`Player Choice = ${playerChoice}`);
+    _computerChoice = getComputerChoice();
+    console.log(`Computer Choice = ${_computerChoice}`);
 
-//create the main function that will help to play the game
-function playRound(_computerChoice = computerChoice, _playerChoice = playerChoice){
-    let result;
     if(_computerChoice === _playerChoice){
         result = "It's a tie";
     }
@@ -33,4 +31,43 @@ function playRound(_computerChoice = computerChoice, _playerChoice = playerChoic
     return result;
 }
 
-console.log(playRound());
+
+let computerWin=0;
+let playerWin=0;
+
+//need this function to play one round five times
+function playGame(){
+    for(i=0 ; i<5 ; i++){
+        console.log(playRound());
+        if(result == "You win"){
+            playerWin += 1;
+        }
+        else if(result == "You lose, Computer wins"){
+            computerWin +=1;
+        }
+        else{
+            playerWin = playerWin;
+            computerWin = computerWin;
+        }
+    }
+}
+//stores the final result
+function getFinalResult(){
+    playGame();
+    console.log("Number of times you won = " + playerWin);
+    console.log("Number of times computer won = " + computerWin);
+    console.log("Number of ties = " + (5-(computerWin + playerWin)));
+
+    let finalResult; //decides the final winner
+    if(playerWin > computerWin){
+        finalResult = "Congrats, You won the game";
+    }
+    else if(computerWin > playerWin){
+        finalResult = "You lost the game, Computer won";
+    }
+    else finalResult = "Entire game is a tie";
+
+    return finalResult;
+}
+
+console.log("Result : " + getFinalResult());
